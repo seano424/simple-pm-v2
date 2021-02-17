ba6d908020f919258bf8bd12752b5f1e0a9f6b11
Sean OReilly <soreilly424@gmail.com>
2021-02-16 21:27:37 -0500

    a simple rails task manager

---
+++ app/models/project.rb
@@ -0,0 +1,42 @@
+class Project < ApplicationRecord
+  has_many :tasks, :dependent => :destroy
+  belongs_to :user
+
+  def badge_color 
+    case status
+    when 'not-started'
+      'secondary'
+    when 'in-progress'
+      'info'
+    when 'completed'
+      'success'
+    end
+  end
+
+  def status
+    return 'not-started' if tasks.none?
+
+    if tasks.all? { |task| task.completed? }
+      'completed'
+    elsif tasks.any? { |task| task.in_progress? }
+      'in-progress'
+    else
+      'not-started'
+    end
+  end
+
+  def percent_completed
+    return 0 if tasks.none?
+
+    ((total_complete.to_f / total_tasks) * 100).round
+  end
+
+  def total_complete
+    tasks.select { |task| task.completed? }.count
+  end
+
+  def total_tasks
+    tasks.count
+  end
+
+end
